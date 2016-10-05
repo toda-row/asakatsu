0d1da932de096e4b22f3e7c90f230f4af8dbe1c0
toda-row <yoshihiro.t.88@gmail.com>
2016-10-05 19:28:53 +0900

    朝活風

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005090723) do
+ActiveRecord::Schema.define(version: 20161005101531) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 20161005090723) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "owner_id"
+    t.string   "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
